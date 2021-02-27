b51ef0fed33e4a05759c2ea0c9271982cea244f9
Thomas Rausch <thomas@thrau.at>
2021-02-27 15:42:01 +0100

    Refactor tools/capture.py to make it reusable

--- tools/capture.py
+++ tools/capture.py
@@ -5,6 +5,38 @@ import time
 import cv2
 
 
+def capture_from_vid(source=0, width=1920, height=1080, warmup=10):
+    """
+    Returns a single frame from the given VideoCapture source.
+
+    Args:
+        source: the VideoCapture source
+        width: frame width
+        height: frame height
+        warmup: number of frames to be discarded before capturing
+
+    Returns: a single frame
+
+    """
+    if warmup < 0:
+        raise ValueError('warmup must be positive, was: %s' % warmup)
+
+    cap = cv2.VideoCapture(source)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    try:
+        frame = None
+        for i in range(warmup + 1):
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+        return frame
+    finally:
+        cap.release()
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--width', type=int, default=1920, help='capture width')
@@ -18,12 +50,9 @@ def main():
     height = args.height
 
     if args.source == '0':
-        cap = cv2.VideoCapture(0)
+        source = 0
     else:
-        cap = cv2.VideoCapture(args.source)
-
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        source = args.source
 
     if args.out:
         if os.path.exists(args.out):
@@ -34,19 +63,10 @@ def main():
         fname = f'cap_{width}x{height}_{int(time.time())}.png'
 
     try:
-        # read 10 frames and then capture (to let the camera focus)
-        for i in range(10):
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-        print(f'recording into {fname}')
+        frame = capture_from_vid(source, width, height)
         cv2.imwrite(fname, frame)
     except KeyboardInterrupt:
         print('interrupted')
-    finally:
-        # Release everything if job is finished
-        cap.release()
 
     print('exiting')
 
